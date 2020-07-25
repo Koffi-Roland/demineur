6b9aed4c402a587c44e306f8e71574dea6e5de92
YAOVI <yaovidjimi@gmail.com>
2020-07-25 15:58:36 +0000

    classe grlle, case et implementation de pattern state

--- main.cpp
+++ main.cpp
@@ -5,12 +5,10 @@
 
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
+
     Grille * grille = Grille::getInstance();
     grille->getCases();
-=======
 
->>>>>>> 446cd8a98cc91e6ce32bbb625c456dc552c51521
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
